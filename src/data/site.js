030532454d03f48b06c5f7cde66069dd2542f736
Tomas Hajek <haak78@Tomas-2.local>
2026-05-02 01:36:57 +0200

    Oprava typografie: nezalomitelne mezery za spojky a cisla

--- src/data/site.js
+++ src/data/site.js
@@ -1,23 +1,23 @@
 export const SITE = {
   name: 'Tomáš Hájek',
-  description: 'Multipotenciál, lektor komunikace a LEGO Education, fotograf a milovník cestování.',
+  description: 'Multipotenciál, lektor komunikace a LEGO Education, fotograf a milovník cestování.',
   author: 'Tomáš Hájek',
 };
 
 export const O_MNE = {
   jmeno: 'Tomáš Hájek',
-  bio: 'Přes 25 let praxe v komunikaci, fotografii a lektorování. Z člověka, který původně "jen fotil", jsem se vypracoval v multipotenciála. Věřím ve smysluplné vzdělávání, ať už jde o starosty menších obcí, nebo pedagogy pracující s dětmi.',
+  bio: 'Přes 25 let praxe v komunikaci, fotografii a lektorování. Z člověka, který původně "jen fotil", jsem se vypracoval v multipotenciála. Věřím ve smysluplné vzdělávání, ať už jde o starosty menších obcí, nebo pedagogy pracující s dětmi.',
   zkusenosti: [
-    '1 ze 6 certifikovaných lektorů LEGO Education v ČR',
-    'Lektor podnikavosti a certifikátor škol (Asociace podnikavé Česko)',
-    'Přes 20 let fotografické praxe (Lidové noviny, Jihomoravský kraj, radnice Brno-střed)',
+    '1 ze 6 certifikovaných lektorů LEGO Education v ČR',
+    'Lektor podnikavosti a certifikátor škol (Asociace podnikavé Česko)',
+    'Přes 20 let fotografické praxe (Lidové noviny, Jihomoravský kraj, radnice Brno-střed)',
     'Správce sociálních sítí Jihomoravského kraje',
-    'Zakladatel webu objevimesvet.cz (cestování s dětmi)',
+    'Zakladatel webu objevimesvet.cz (cestování s dětmi)',
   ],
   stats: [
-    { cislo: '230+', popisek: 'Proškolených starostů v loňském roce' },
-    { cislo: '600+', popisek: 'Proškolených učitelů s LEGO Education' },
-    { cislo: '25+', popisek: 'Let praxe v komunikaci a foto' }
+    { cislo: '230+', popisek: 'Proškolených starostů v loňském roce' },
+    { cislo: '600+', popisek: 'Proškolených učitelů s LEGO Education' },
+    { cislo: '25+', popisek: 'Let praxe v komunikaci a foto' }
   ]
 };
 
@@ -25,31 +25,31 @@ export const PILIRE = [
   {
     id: 'skoleni',
     nazev: 'Školení komunikace',
-    popis: 'Sociální sítě, focení mobilem a digitální dovednosti pro moderní obce a neziskovky. Prakticky, s nadhledem a bez zbytečné teorie.',
+    popis: 'Sociální sítě, focení mobilem a digitální dovednosti pro moderní obce a neziskovky. Prakticky, s nadhledem a bez zbytečné teorie.',
     odkaz: '/skoleni'
   },
   {
     id: 'lego',
     nazev: 'LEGO Education',
-    popis: 'Využijte potenciál LEGO Education na maximum. Jako certifikovaný lektor propojuji hru s robotikou, vědou a moderními technologiemi.',
+    popis: 'Využijte potenciál LEGO Education na maximum. Jako certifikovaný lektor propojuji hru s robotikou, vědou a moderními technologiemi.',
     odkaz: '/lego'
   },
   {
     id: 'prototypci',
     nazev: 'Prototýpci & Vzdělávání',
-    popis: 'Rozvíjíme podnikavost a kreativitu u dětí. Od robotiky s Ozoboty až po inovativní metodiku pro školy 21. století.',
+    popis: 'Rozvíjíme podnikavost a kreativitu u dětí. Od robotiky s Ozoboty až po inovativní metodiku pro školy 21. století.',
     odkaz: '/prototypci'
   },
   {
     id: 'foto',
     nazev: 'Fotografie & Obrazy',
-    popis: 'Dvě dekády zkušeností za hledáčkem. Od reportáží pro velká média až po limitované autorské tisky pro váš interiér.',
+    popis: 'Dvě dekády zkušeností za hledáčkem. Od reportáží pro velká média až po limitované autorské tisky pro váš interiér.',
     odkaz: '/foto'
   },
   {
     id: 'projekty',
     nazev: 'Projekty & Vášeň',
-    popis: 'Cestování s dětmi, komunita kolem hodinek Prim nebo edukace o fotovoltaice. Život je příliš krátký na jedinou profesi.',
+    popis: 'Cestování s dětmi, komunita kolem hodinek Prim nebo edukace o fotovoltaice. Život je příliš krátký na jedinou profesi.',
     odkaz: '/projekty'
   }
 ];
